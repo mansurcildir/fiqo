ef66d268637434f296393c412d32d180cc723112
mansurcildir <mansurcildir@gmail.com>
2025-09-21 23:40:22 +0300

    fiqo panel tail admin react template, auth operations

---
+++ fiqo-panel/README.md
@@ -0,0 +1,55 @@
+# fiqo-panel
+
+Frontend web application of fiqo for panel
+
+## Features
+
+- lightweight dashboard which you can manage your file operations
+
+## Tech Stack
+
+**Language:** TypeScript
+
+**Framework:** React
+
+**UI:** TailAdmin
+
+**CSS Frameworks:** TailwindCSS
+
+## Run Locally
+
+- Clone the project
+
+```bash
+  git clone https://github.com/mansurcildir/fiqo.git
+```
+
+- Go to the project directory
+
+```bash
+  cd fiqo/fiqo-panel
+```
+
+- Install dependencies
+
+```bash
+   #pnpm
+   pnpm install
+```
+
+```bash
+   #npm
+   npm install
+```
+
+- Run application in development
+
+```bash
+   #pnpm
+   pnpm run dev
+```
+
+```bash
+   #npm
+   npm run dev
+```
---
+++ fiqo-panel/src/pages/UserProfiles.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { userAPI } from '../service/user-service';
+import PageBreadcrumb from '../components/common/PageBreadCrumb';
+import UserMetaCard from '../components/UserProfile/UserMetaCard';
+import UserInfoCard from '../components/UserProfile/UserInfoCard';
+import UserAddressCard from '../components/UserProfile/UserAddressCard';
+import PageMeta from '../components/common/PageMeta';
+
+export default function UserProfiles() {
+  const [userInfo, setUserInfo] = useState({
+    uuid: '',
+    username: '',
+    email: ''
+  });
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      userAPI.getProfile().then((res) => {
+        setUserInfo({
+          uuid: res.data.uuid,
+          username: res.data.username,
+          email: res.data.email
+        });
+      });
+    };
+
+    fetchProfile();
+  }, []);
+
+  return (
+    <>
+      <PageMeta
+        title="React.js Profile Dashboard | TailAdmin - Next.js Admin Dashboard Template"
+        description="This is React.js Profile Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+      />
+      <PageBreadcrumb pageTitle="Profile" />
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 lg:p-6 dark:border-gray-800 dark:bg-white/[0.03]">
+        <h3 className="mb-5 text-lg font-semibold text-gray-800 lg:mb-7 dark:text-white/90">Profile</h3>
+        <div className="space-y-6">
+          <UserMetaCard username={userInfo.username} />
+          <UserInfoCard email={userInfo.email} />
+          <UserAddressCard />
+        </div>
+      </div>
+    </>
+  );
+}

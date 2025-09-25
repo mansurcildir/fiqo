8536375fd623eac76b1b58d7d6ec3021967454ee
mansurcildir <mansurcildir@gmail.com>
2025-09-25 15:20:46 +0300

    file explorer and oauth2

--- fiqo-panel/src/pages/UserProfiles.tsx
+++ fiqo-panel/src/pages/UserProfiles.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { userAPI } from '../service/user-service';
-import PageBreadcrumb from '../components/common/PageBreadCrumb';
 import UserMetaCard from '../components/UserProfile/UserMetaCard';
 import UserAccount from '../components/UserProfile/UserAccount';
 import PageMeta from '../components/common/PageMeta';
@@ -32,7 +31,6 @@ export default function UserProfiles() {
         title="React.js Profile Dashboard | TailAdmin - Next.js Admin Dashboard Template"
         description="This is React.js Profile Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="Profile" />
       <div className="rounded-2xl border border-gray-200 bg-white p-5 lg:p-6 dark:border-gray-800 dark:bg-white/[0.03]">
         <h3 className="mb-5 text-lg font-semibold text-gray-800 lg:mb-7 dark:text-white/90">Profile</h3>
         <div className="space-y-6">
